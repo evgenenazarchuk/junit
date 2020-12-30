fe8e6435068af2cd4068bbacd64e3a2ebda3130e
e.nazarchuk <e.nazarchuk@gmail.com>
2020-12-30 19:35:15 +0300

    first commit;)1

--- src/test/java/by/academy/homework/TestSuite/SuiteTest.java
+++ src/test/java/by/academy/homework/TestSuite/SuiteTest.java
@@ -2,14 +2,13 @@ package by.academy.homework.TestSuite;
 
 import by.academy.CalculatorTest;
 import by.academy.SecondClassTest;
-import by.academy.homework.ApplicationListener;
 import by.academy.homework.Parametrize.ParameterizeTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 
     @RunWith(Suite.class)
-    @Suite.SuiteClasses({CalculatorTest.class, SecondClassTest.class, ParameterizeTest.class, ApplicationListener.class})
+    @Suite.SuiteClasses({CalculatorTest.class, SecondClassTest.class, ParameterizeTest.class})
     public class SuiteTest  {
 
     }
